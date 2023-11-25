c4fe9f29d91b0baf9a30e5333e68d7fe76fcef57
yopiangga <yopigambyok@gmail.com>
2023-11-25 12:56:59 +0700

    example POST

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import numpy
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def hello_world():
 	return 'Hello Team Mata Netra | Auto Deploy follow Branch Master 2'
+
+@app.route('/message', methods=['POST'])
+@cross_origin()
+def post_example():
+	data = request.get_json()
+	message = data.get('message', 'No message provided')
+	response = {'status': 'success', "result_text": message}
+	return jsonify(response)
 
 # main driver function
 if __name__ == '__main__':
