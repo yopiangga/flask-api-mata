3b4c027fd13ffd314482d3c31ecb4f6d9ad170b8
Yus A <yus30adalmahmudi@gmail.com>
2023-11-26 22:22:15 +0700

    test lagi

--- method/image_classifiation.py
+++ method/image_classifiation.py
@@ -1,31 +1,35 @@
 # Step 1: Import necessary libraries
-from keras.datasets import cifar10
-from keras.models import Sequential
-from keras.layers import Dense, Flatten
-from keras.utils import to_categorical
+import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
 import tensorflow as tf
 
-# Step 2: Load the dataset
-(train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
+# Step 2: Load the CSV file using pandas
+df = pd.read_csv('data-d4-l1-v2')
 
-# Step 3: Preprocess the dataset
-train_images = train_images / 255.0
-test_images = test_images / 255.0
-train_labels = to_categorical(train_labels)
-test_labels = to_categorical(test_labels)
+# Step 3: Preprocess the dataset if necessary
+# This step depends on your dataset. You might need to normalize numerical data, 
+# encode categorical data, handle missing values, etc.
 
-# Step 4: Define the model architecture
+# Step 4: Split the dataset into features (X) and target (y)
+X = df.drop('target_column', axis=1)
+y = df['target_column']
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Step 5: Define the model architecture
 model = Sequential([
-    Flatten(input_shape=(32, 32, 3)),
-    Dense(64, activation='relu'),
-    Dense(10, activation='softmax')
+    Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
+    Dense(1, activation='sigmoid')
 ])
 
-# Step 5: Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+# Step 6: Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# Step 6: Train the model with the dataset
-model.fit(train_images, train_labels, epochs=10, batch_size=64)
+# Step 7: Train the model with the dataset
+model.fit(X_train, y_train, epochs=10, batch_size=32)
 
-# Step 7: Save the model in .h5 format
+# Step 8: Save the model in .h5 format
 model.save('model.h5')
