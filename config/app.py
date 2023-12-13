a1728f542e841e8f4bdd5be7098a113629b45dfa
yopiangga <yopigambyok@gmail.com>
2023-12-14 00:36:44 +0700

    change path relative

--- config/app.py
+++ config/app.py
@@ -1,3 +1,5 @@
 
-path = "C:/Users/dismo/OneDrive/Documents/Alfian Prisma Yopiangga/Bangkit/Capstone/code/flask-api-mata/"
+# path = "C:/Users/dismo/OneDrive/Documents/Alfian Prisma Yopiangga/Bangkit/Capstone/code/flask-api-mata/"
+# path = "C:/Users/akuns/Downloads/yopi/Bangkit/code/flask-api-mata/"
+path = "/var/www/flask-api-mata/"
 version = "1.0.0"
